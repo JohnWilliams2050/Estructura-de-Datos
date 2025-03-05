f85e7d61550ed352b59510b600c227a34495b33e
JohnWilliams2050 <109265994+JohnWilliams2050@users.noreply.github.com>
2025-03-05 18:35:14 -0500

    Update modulo.cpp

--- Laboratorio_Arboles/modulos/modulo.cpp
+++ Laboratorio_Arboles/modulos/modulo.cpp
@@ -3,7 +3,7 @@
 #include "modulo.h"
 
 using namespace std;
-//
+//Revisa que el nodo no sea NULL. Despues imprime la raíz, despues el subárbol izquierdo, y finalmente imprime el subárbol derecho.
 void preOrdenT(struct Nodo *nodo){
     if (nodo==NULL){
         return;
@@ -12,7 +12,7 @@ void preOrdenT(struct Nodo *nodo){
     preOrdenT(nodo->left);
     preOrdenT(nodo->right);
 }
-//
+//Revisa que el nodo no sea NULL. Despues imprime el subárbol izquierdo, imprime el subárbol derecho, y finalmente imprime la raíz.
 void postOrdenT(struct Nodo *nodo)
 {
     if (nodo==NULL){
@@ -22,7 +22,7 @@ void postOrdenT(struct Nodo *nodo)
     postOrdenT(nodo->right);
     cout<< nodo->dato<<"->";
 }
-
+//Revisa que el nodo no sea NULL. Despues imprime subárbol izquierdo, despues imprime la raíz, y finalmente imprime el subárbol derecho.
 void inOrdenT(struct Nodo *nodo)
 {
     if (nodo==NULL){
