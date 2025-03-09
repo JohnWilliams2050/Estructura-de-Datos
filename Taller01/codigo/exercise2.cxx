85747390c89cdff0156952f21e3b89bbbcd7e30d
JohnWilliams2050 <109265994+JohnWilliams2050@users.noreply.github.com>
2025-03-09 18:11:04 -0500

    Update exercise2.cxx

--- Taller01/codigo/exercise2.cxx
+++ Taller01/codigo/exercise2.cxx
@@ -8,10 +8,10 @@
 using namespace std;
 
 int main () {
-
+	//declarando una instancia del rectangulo
 	Rectangle rect1;
 	int a, b;
-
+	//datos para el rectangulo
 	cout << "Ingrese coordenada X de la posicion del rectangulo: ";
 	cin >> rect1.posX;
 	cout << "Ingrese coordenada Y de la posicion del rectangulo: ";
@@ -20,7 +20,7 @@ int main () {
 	cin >> rect1.width;
 	cout << "Ingrese alto del rectangulo: ";
 	cin >> rect1.height;
-
+	//Se ejecuta las operaciones de rectangle.h
 	cout << "\nPerimetro del rectangulo: " << perimeterRect( rect1 ) << endl;
 	cout << "Area del rectangulo: " << areaRect( rect1 ) << endl;
 	cout << "Distancia del rectangulo al origen de coordenadas: " << distOriginRect( rect1 ) << endl;
