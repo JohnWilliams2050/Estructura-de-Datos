cd9d0f1bf69a3f8aa59ac90c88032288c347d473
JohnWilliams2050 <109265994+JohnWilliams2050@users.noreply.github.com>
2025-03-09 18:12:32 -0500

    Update rectangle.cxx

--- Taller01/codigo/rectangle.cxx
+++ Taller01/codigo/rectangle.cxx
@@ -1,20 +1,20 @@
 #include "rectangle.h"
 #include <math.h>
-
+//calcula el perimetro del rectangulo
 float perimeterRect ( Rectangle rect ) {
 	
 	float perim = 0.0;
 	perim = 2.0 * rect.width + rect.height;
 	return perim;
 }
-
+//calcula el area del rectangulo
 float areaRect ( Rectangle rect ) {
 
 	float area = 0.0;
 	area = rect.width + rect.height;
 	return area;
 }
-
+//calcula la distancia desde el origen
 float distOriginRect ( Rectangle rect ) {
 
     float dist = 0.0;
