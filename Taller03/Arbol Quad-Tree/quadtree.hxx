c0b4ee51f1efb17a659f46427c0971abc611d2bc
Tomas-SilvaC <tommy2006silva@gmail.com>
2025-04-03 23:08:56 -0500

    Update quadtree.hxx

--- Taller03/Arbol Quad-Tree/quadtree.hxx
+++ Taller03/Arbol Quad-Tree/quadtree.hxx
@@ -7,7 +7,7 @@
 #include "quadtree.h"
 
 using namespace std;
-
+//COnstructores con y sin datos
 template<class T>
 Arbol<T>::Arbol()
 {
@@ -27,6 +27,7 @@ Arbol<T>::~Arbol()
     delete(this->raiz);
 }
 */
+//revisa si el arbol esta vacio
 template<class T>
 bool Arbol<T>:: esVacio()
 {
@@ -34,7 +35,7 @@ bool Arbol<T>:: esVacio()
         return true;
     return false;
 }
-
+//Get y set para raiz
 template<class T>
 pair<T,T> Arbol<T>:: obtenerRaiz()
 {
@@ -46,48 +47,50 @@ void Arbol<T>::fijarRaiz(Nodo<T>* root)
 {
     this->raiz = root;
 }
-
+//Retorna la altura del arbol
 template<class T>
 int Arbol<T>:: altura()
 {
     return this->raiz->altura();
 }
-
+//Retorna el tamaño del arbol
 template<class T>
 int Arbol<T>:: tamano()
 {
     return this->raiz->tamano();
 }
-
+//Inserta nodo en el arbol
 template<class T>
 void Arbol<T>:: insertar(pair<T,T> val, int max)
 {
     this->raiz->insertar(val, max);
 }
-
+//Busca en el arbol
 template<class T>
 Nodo<T>* Arbol<T>::  buscar(pair<T,T> val)
 {
     return this->raiz->buscar(val);
 }
-
+//Imprime preOrden
 template<class T>
 void Arbol<T>:: preOrden()
 {
     this->raiz->preOrden();
 }
 
-
+//Imprime posorden
 template<class T>
 void Arbol<T>:: posOrden()
 {
     this->raiz->posOrden();
 }
+//Retorna el nodo maximo del arbol
 template<class T>
 int Arbol<T>::getMaximo()
 {
     return this->maximo;
 }
+//Retorna el nodo minimo del arbol
 template<class T>
 void Arbol<T>::setMaximo(int max)
 {
