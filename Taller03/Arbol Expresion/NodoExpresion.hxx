10095f533fa4b641bde1dfb3ad1e394207d0b7ad
Tomas-SilvaC <tommy2006silva@gmail.com>
2025-04-03 23:12:11 -0500

    Update NodoExpresion.hxx

--- Taller03/Arbol Expresion/NodoExpresion.hxx
+++ Taller03/Arbol Expresion/NodoExpresion.hxx
@@ -1,15 +1,20 @@
+/**
+* Taller 3. Arboles
+* Autores: Tomas Silva, Juan Pabon Vargas
+* nodo Expresión
+**/
 #include "NodoExpresion.h"
 #include <string>
 
 using namespace std;
-
+//Constructores 
 NodoExpresion::NodoExpresion(){
 	this->hijoIzq=nullptr;
 	this->hijoDer=nullptr;
 }
 NodoExpresion::~NodoExpresion(){
 }
-
+//Get y set para dato
 char NodoExpresion::getDato(){
 	return this->dato;
 }
@@ -18,7 +23,7 @@ void NodoExpresion::setDato(char val){
 	this->dato=val;
 	return;
 }
-
+//Get y set para los hijos derecho e izquierdo
 NodoExpresion* NodoExpresion::getHijoIzq(){
 	return this->hijoIzq;
 }
@@ -27,10 +32,16 @@ NodoExpresion* NodoExpresion::getHijoDer(){
 	return this->hijoDer;
 }
 
+void NodoExpresion::setHijoDer(NodoExpresion* der){
+	this->hijoDer=der;
+	return;
+}
+
 void NodoExpresion::setHijoIzq(NodoExpresion* izq){
 	this->hijoIzq=izq;
 	return;
 }
+//Get y set para el operando
 void NodoExpresion::setOperando(bool op){
 	this->operando=op;
 	return;
@@ -38,7 +49,5 @@ void NodoExpresion::setOperando(bool op){
 bool NodoExpresion::getOperando(){
 	return this->operando;
 }
-void NodoExpresion::setHijoDer(NodoExpresion* der){
-	this->hijoDer=der;
-	return;
-}
+
+
